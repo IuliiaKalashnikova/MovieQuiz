a97cab390427f100dbb155f49f69f24772b71905
Iuliia Kalashnikova <julia_kalashnikova01@mail.ru>
2024-11-24 00:31:51 +0300

    Спринт 7

--- MovieQuizUITests1/MovieQuizUITests1.swift
+++ MovieQuizUITests1/MovieQuizUITests1.swift
@@ -20,22 +20,20 @@ final class MovieQuizUITests1: XCTestCase {
         continueAfterFailure = true
     }
     
-   func testYesButton() {
-       sleep(3)
-       
-          
-          let firstPoster = app.images["Poster"]
-          let firstPosterData = firstPoster.screenshot().pngRepresentation
-          
-          app.buttons["Yes"].tap()
-          sleep(3)
-          
-          let secondPoster = app.images["Poster"]
-          let secondPosterData = secondPoster.screenshot().pngRepresentation
-       
-       XCTAssertNotEqual(firstPosterData, secondPosterData)
-          
-      }
+    func testYesButton() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+    }
     func testNoButton() {
         sleep(3)
         
@@ -47,10 +45,10 @@ final class MovieQuizUITests1: XCTestCase {
         
         let secondPoster = app.images["Poster"]
         let secondPosterData = secondPoster.screenshot().pngRepresentation
-
+        
         let indexLabel = app.staticTexts["Index"]
-       
-        XCTAssertFalse(firstPoster == secondPoster)
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
         XCTAssertEqual(indexLabel.label, "2/10")
     }
     func testGameFinish() {
@@ -59,14 +57,14 @@ final class MovieQuizUITests1: XCTestCase {
             app.buttons["No"].tap()
             sleep(2)
         }
-
+        
         let alert = app.alerts["Alert"]
         
         XCTAssertTrue(alert.exists)
         XCTAssertTrue(alert.label == "Этот раунд окончен!")
         XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть ещё раз")
     }
-
+    
     func testAlertDismiss() {
         sleep(2)
         for _ in 1...10 {
